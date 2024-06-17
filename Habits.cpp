ce0320bf55aa959604ba653db300feda6391c9a5
Md Shahzad Shamim <shahzad.shamim@hotmail.com>
2024-06-17 16:43:46 +0530

    Added Functionality - Today's Status

--- Habits.cpp
+++ Habits.cpp
@@ -32,12 +32,14 @@ int main()
         while(repeat != 'n')
         {
             int habit = 0, sum1 = 22, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0;
+            int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0;
+            int count = 0;
 
             system("cls");
 
             pqxx::work txnq(conn);
             pqxx::result resultq = txnq.exec("SELECT SUM(discrete_maths), SUM(leet_code_1), SUM(systems_design), "
-                                                    "SUM(leet_code_2), SUM(cpp), SUM(leet_code_3) FROM habits");
+                                                    "SUM(leet_code_2), SUM(computer_networks), SUM(leet_code_3) FROM habits");
             txnq.commit();
 
             // SELECT SUM(discrete_maths), SUM(leet_code_1), SUM(systems_design), SUM(leet_code_2), SUM(cpp), SUM(leet_code_3) FROM habits;
@@ -49,14 +51,34 @@ int main()
             sum5 += resultq[0][4].as<int>();
             sum6 += resultq[0][5].as<int>();
 
+            // -----------------------------counted sterak
+
+            pqxx::work txnc(conn);
+            pqxx::result resultc = txnc.exec("SELECT discrete_maths, leet_code_1, systems_design, leet_code_2, "
+                                                    "computer_networks, leet_code_3 FROM habits WHERE date_ymd = CURRENT_DATE");
+            txnc.commit();
+
+            n1 = resultc[0][0].as<int>();
+            n2 = resultc[0][1].as<int>();
+            n3 = resultc[0][2].as<int>();
+            n4 = resultc[0][3].as<int>();
+            n5 = resultc[0][4].as<int>();
+            n6 = resultc[0][5].as<int>();
+
+            // -----------------------------accessed todays data
+
             cout << endl << "\n\n\n\t\t\t\tH\tA\tB\tI\tT\tS\n\n\n";
 
-            cout << setw(30) << left << "\t\t\t\t1 - Discrete Mathematics" << sum1 << endl;
-            cout << setw(30) << left << "\t\t\t\t2 - LeetCode Day 1" << sum2 << endl;
-            cout << setw(30) << left << "\t\t\t\t3 - Systems Design" << sum3 << endl;
-            cout << setw(30) << left << "\t\t\t\t4 - LeetCode Day 2" << sum4<< endl;
-            cout << setw(30) << left << "\t\t\t\t5 - C++ Programming" << sum5 << endl;
-            cout << setw(30) << left << "\t\t\t\t6 - LeetCode Day 3" << sum6 << endl;
+            cout << setw(33) << left << "\t\t\t\t#   SUBJECT"               << setw(10) << left << "STREAK" <<  "TODAY\n" << endl;
+
+            cout << setw(35) << left << "\t\t\t\t1 - Discrete Mathematics"  << setw(10) << left << sum1       << n1       << endl;
+            cout << setw(35) << left << "\t\t\t\t2 - LeetCode Day 1"        << setw(10) << left << sum2       << n2       << endl;
+            cout << setw(35) << left << "\t\t\t\t3 - Systems Design"        << setw(10) << left << sum3       << n3       << endl;
+            cout << setw(35) << left << "\t\t\t\t4 - LeetCode Day 2"        << setw(10) << left << sum4       << n4       << endl;
+            cout << setw(35) << left << "\t\t\t\t5 - Computer Networks"     << setw(10) << left << sum5       << n5       << endl;
+            cout << setw(35) << left << "\t\t\t\t6 - LeetCode Day 3"        << setw(10) << left << sum6       << n6       << endl;
+            cout << endl;
+            cout << setw(35) << left << "\t\t\t\tX - LeetCode Total"        << setw(10) << left << sum2+sum4+sum6         << endl;
 
             // Note - It's a personalized app so you need to change the habit names here 
             // And in the database, if you want to access it using sql directly
@@ -73,7 +95,11 @@ int main()
                     txn1.commit();
                     break;
                 case 2:
-                    txn1.exec("UPDATE habits SET leet_code_1 = 1 WHERE date_ymd = CURRENT_DATE");
+                    cout << "\n\n\t\t\t\tCount:\t\t";
+                    count = 0;
+                    cin >> count;
+                    clearInputBuffer();
+                    txn1.exec_params("UPDATE habits SET leet_code_1 = $1 WHERE date_ymd = CURRENT_DATE", count);
                     txn1.commit();
                     break;
                 case 3:
@@ -81,15 +107,23 @@ int main()
                     txn1.commit();
                     break;
                 case 4:
-                    txn1.exec("UPDATE habits SET leet_code_2 = 1 WHERE date_ymd = CURRENT_DATE");
+                    cout << "\n\n\t\t\t\tCount:\t\t";
+                    count = 0;
+                    cin >> count;
+                    clearInputBuffer();
+                    txn1.exec_params("UPDATE habits SET leet_code_2 = $1 WHERE date_ymd = CURRENT_DATE", count);
                     txn1.commit();
                     break;
                 case 5:
-                    txn1.exec("UPDATE habits SET cpp = 1 WHERE date_ymd = CURRENT_DATE");
+                    txn1.exec("UPDATE habits SET computer_networks = 1 WHERE date_ymd = CURRENT_DATE");
                     txn1.commit();
                     break;
                 case 6:
-                    txn1.exec("UPDATE habits SET leet_code_3 = 1 WHERE date_ymd = CURRENT_DATE");
+                    cout << "\n\n\t\t\t\tCount:\t\t";
+                    count = 0;
+                    cin >> count;
+                    clearInputBuffer();
+                    txn1.exec_params("UPDATE habits SET leet_code_3 = $1 WHERE date_ymd = CURRENT_DATE", count);
                     txn1.commit();
                     break;
                 default:
